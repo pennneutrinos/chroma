399e6fd9e5a29d5fe9ea993a971bf8285da4b293
James <jieran.shen@duke.edu>
2022-07-05 23:16:12 -0400

    update docker workdir

--- start_docker.sh
+++ start_docker.sh
@@ -3,4 +3,6 @@ docker run -it --gpus all\
   -v /media/linux_store/hep:/media/linux_store/hep \
   -v /media/linux_store/hep/chroma/chroma:/opt/chroma/chroma \
   -v /tmp/.X11-unix:/tmp/.X11-unix\
+  --network host \
+  --workdir='/media/linux_store/hep/' \
   stjimmys/chroma3:nvidia.cuda10
