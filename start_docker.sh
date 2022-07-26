05e797af7f997bbd8f21e51c14be6e1b375f0435
James <jieran.shen@duke.edu>
2022-07-26 15:46:36 -0400

    X11 Passthrough

--- start_docker.sh
+++ start_docker.sh
@@ -1,8 +1,10 @@
+sudo cp $XAUTHORITY /media/linux_store/hep/container_home/.Xauthority
 docker run -it --rm --gpus all\
   -v /media/linux_store/hep/container_home:/root \
   -v /media/linux_store/hep:/media/linux_store/hep \
   -v /media/linux_store/hep/chroma/chroma:/opt/chroma/chroma \
   -v /tmp/.X11-unix:/tmp/.X11-unix\
+  --env="DISPLAY"\
   --network host \
   --workdir='/media/linux_store/hep/' \
   stjimmys/chroma3:nvidia.cuda10
