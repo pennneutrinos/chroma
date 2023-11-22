2b40d98ef25c10764ef8197e9a0f22bda5d97a92
James Shen <jamesjieranshen@gmail.com>
2023-11-22 18:37:05 -0500

    reformat loader

--- chroma/gdml/loader.py
+++ chroma/gdml/loader.py
@@ -8,34 +8,36 @@ from chroma.geometry import Mesh, Solid
 from chroma.log import logger
 from copy import deepcopy
 
-#Using the PyMesh library instead of Chroma's internal meshes to support boolean
-#operations like subtractions or unions. Perhaps Chroma's meshes should be
-#replaced _entirely_ by PyMesh...
+# Using the PyMesh library instead of Chroma's internal meshes to support boolean
+# operations like subtractions or unions. Perhaps Chroma's meshes should be
+# replaced _entirely_ by PyMesh...
 from chroma.gdml import loader_helper as helper
 from chroma.gdml import gen_mesh
 import gmsh
 
-    
+
 def retrieve_mesh_from_gmsh():
     faceTags, nodeTags = gmsh.model.mesh.getElementsByType(2)
     # three nodetags coorespond to one facetag
     faces = np.asarray(nodeTags)
-    faces -= 1 # because tags are 1-indexed
+    faces -= 1  # because tags are 1-indexed
     faces = np.reshape(faces, (-1, 3))
     nodeTags, coords, _ = gmsh.model.mesh.getNodes()
     coords = np.reshape(coords, (-1, 3))
     return coords, faces
 
 
-#To convert length and angle units to cm and radians
-units = { 'cm':1, 'mm':0.1, 'm':100, 'deg':np.pi/180, 'rad':1 }
+# To convert length and angle units to cm and radians
+units = {'cm': 1, 'mm': 0.1, 'm': 100, 'deg': np.pi / 180, 'rad': 1}
+
 
 class Volume:
     '''
     Represents a GDML volume and the volumes placed inside it (physvol) as 
     childeren. Keeps track of position and rotation of the GDML solid.
     '''
-    def __init__(self,name,gdml):
+
+    def __init__(self, name, gdml):
         self.name = name
         elem = gdml.vol_map[name]
         self.material_ref = elem.find('materialref').get('ref')
@@ -50,25 +52,31 @@ class Volume:
             self.children.append(vol)
             self.child_pos.append(pos)
             self.child_rot.append(rot)
-    def show_hierarchy(self,indent=''):
-        print(indent+str(self), self.solid,self.material_ref)
+
+    def show_hierarchy(self, indent=''):
+        print(indent + str(self), self.solid, self.material_ref)
         for child in self.children:
-            child.show_hierarchy(indent=indent+' ')
+            child.show_hierarchy(indent=indent + ' ')
+
     def __str__(self):
         return self.name
+
     def __repr__(self):
         return str(self)
 
+
 from chroma.demo.optics import vacuum
+
+
 def _default_volume_classifier(volume_ref, material_ref, parent_material_ref):
     '''This is an example volume classifier, primarily for visualization'''
     if 'OpDetSensitive' in volume_ref:
-        return 'pmt',dict(material1=vacuum, material2=vacuum, color=0xA0A05000, surface=None, channel_type=0)
+        return 'pmt', dict(material1=vacuum, material2=vacuum, color=0xA0A05000, surface=None, channel_type=0)
     elif material_ref == parent_material_ref:
-        return 'omit',dict()
+        return 'omit', dict()
     else:
-        return 'solid',dict(material1=vacuum, material2=vacuum, color=0xEEA0A0A0, surface=None)
-        
+        return 'solid', dict(material1=vacuum, material2=vacuum, color=0xEEA0A0A0, surface=None)
+
 
 class GDMLLoader:
     '''
@@ -76,41 +84,40 @@ class GDMLLoader:
     the XML. A subset of GDML is supported here, and exceptions will be raised
     if the GDML uses unsupported features.
     '''
-    
+
     def __init__(self, gdml_file, refinement_order=0):
         ''' 
         Read a geometry from the specified GDML file.
         '''
         self.refinement_order = refinement_order
-        
+
         self.gdml_file = gdml_file
         xml = et.parse(gdml_file)
         gdml = xml.getroot()
-        
+
         define = gdml.find('define')
-        self.pos_map = { pos.get('name'):pos for pos in define.findall('position') }
-        self.rot_map = { rot.get('name'):rot for rot in define.findall('rotation') }
-        
+        self.pos_map = {pos.get('name'): pos for pos in define.findall('position')}
+        self.rot_map = {rot.get('name'): rot for rot in define.findall('rotation')}
+
         solids = gdml.find('solids')
-        self.solid_map = { solid.get('name'):solid for solid in solids }
-        
+        self.solid_map = {solid.get('name'): solid for solid in solids}
+
         structure = gdml.find('structure')
         volumes = structure.findall('volume')
-        self.vol_map = { v.get('name'):v for v in volumes }
-        
+        self.vol_map = {v.get('name'): v for v in volumes}
+
         world_ref = gdml.find('setup').find('world').get('ref')
         self.world = Volume(world_ref, self)
         self.mesh_cache = {}
 
         ## Initialize gmsh
         gmsh.initialize()
-        gmsh.option.setNumber('Mesh.MeshSizeFromCurvature', 32) # number of meshes per 2*pi radian
-        gmsh.option.setNumber('Mesh.MinimumCircleNodes', 32) # number of nodes per circle
+        gmsh.option.setNumber('Mesh.MeshSizeFromCurvature', 32)  # number of meshes per 2*pi radian
+        gmsh.option.setNumber('Mesh.MinimumCircleNodes', 32)  # number of nodes per circle
         gmsh.option.setNumber('General.Verbosity', 2)
         gmsh.option.setNumber('Geometry.ToleranceBoolean', 0.001)
         gmsh.model.add(self.gdml_file)
 
-        
     def get_pos_rot(self, elem, refs=('position', 'rotation')):
         ''' 
         Searches for position and rotation children of an Element. The found
@@ -118,7 +125,7 @@ class GDMLLoader:
         defined inline as position, rotation tags, and dereferences 
         positionref, rotationref using defined values. Returns None if 
         neither inline nor ref is specified.
-        '''           
+        '''
         pos_ref, rot_ref = refs
         pos = elem.find(pos_ref)
         if pos is None:
@@ -130,27 +137,9 @@ class GDMLLoader:
             rot = elem.find(rot_ref + 'ref')
             if rot is not None:
                 rot = self.rot_map[rot.get('ref')]
-        return pos,rot
-        
-    def get_vals(self, elem, value_attr=['x', 'y', 'z'], unit_attr='unit'):
-        '''
-        Calls get_val for a list of attributes (value_attr). The result 
-        values are scaled from the unit specified in the unit_attrib attribute.
-        '''
-        scale = units[elem.get(unit_attr)] if unit_attr is not None else 1.0
-        return [self.get_val(elem, attr) * scale for attr in value_attr]
-        
-    def get_val(self, elem, attr, default=None):
-        '''
-        Calls eval on the value of the attribute attr if it exists and return 
-        it. Otherwise return the default specified. If there is no default 
-        specifed, raise an exception.
-        '''
-        txt = elem.get(attr, default=None)
-        assert txt is not None or default is not None, 'Missing attribute: '+attr
-        return eval(txt, {}, {}) if txt is not None else default
-        
-    def get_mesh(self,solid_ref):
+        return pos, rot
+
+    def get_mesh(self, solid_ref):
         '''
         Build a PyMesh mesh for the solid identified by solid_ref if the named
         solid has not been built. If it has been built, a cached mesh is returned.
@@ -169,32 +158,34 @@ class GDMLLoader:
             fpos, frot = self.get_pos_rot(elem, refs=('firstposition', 'firstrotation'))
             pos, rot = self.get_pos_rot(elem)
             posrot_entries = (fpos, frot, pos, rot)
-            posrot_vals = [None]*4
+            posrot_vals = [None] * 4
             for i, entry in enumerate(posrot_entries):
                 if entry is not None:
                     posrot_vals[i] = helper.get_vals(entry)
             noUnion = self.noUnionClassifier(solid_ref)
             logger.info(f"Performing {mesh_type} for {solid_ref}")
-            mesh = gen_mesh.gdml_boolean(a, b, mesh_type, firstpos=posrot_vals[0], firstrot=posrot_vals[1], pos=posrot_vals[2], rot=posrot_vals[3], noUnion=noUnion)
+            mesh = gen_mesh.gdml_boolean(a, b, mesh_type, firstpos=posrot_vals[0], firstrot=posrot_vals[1],
+                                         pos=posrot_vals[2], rot=posrot_vals[3], noUnion=noUnion)
             return mesh
         dispatcher = {
-            'box':              helper.box,
-            'eltube':           helper.eltube,
-            'ellipsoid':        helper.ellipsoid,
-            'orb':              helper.orb,
-            'polycone':         helper.polycone,
-            'polyhedra':        helper.polyhedra,
-            'sphere':           helper.sphere,
-            'torus':            helper.torus,
-            'tube':             helper.tube,
-            'torusstack':       helper.torusstack,
-            'opticalsurface':   helper.ignore,
+            'box': helper.box,
+            'eltube': helper.eltube,
+            'ellipsoid': helper.ellipsoid,
+            'orb': helper.orb,
+            'polycone': helper.polycone,
+            'polyhedra': helper.polyhedra,
+            'sphere': helper.sphere,
+            'torus': helper.torus,
+            'tube': helper.tube,
+            'torusstack': helper.torusstack,
+            'opticalsurface': helper.ignore,
         }
         generator = dispatcher.get(mesh_type, helper.notImplemented)
         mesh = generator(elem)
         return mesh
-        
-    def build_detector(self, detector=None, volume_classifier=_default_volume_classifier, solidsToIgnore=None, noUnion=None):
+
+    def build_detector(self, detector=None, volume_classifier=_default_volume_classifier, solidsToIgnore=None,
+                       noUnion=None):
         '''
         Add the meshes defined by this GDML to the detector. If detector is not
         specified, a new detector will be created.
@@ -210,10 +201,10 @@ class GDMLLoader:
         '''
         if detector is None:
             detector = Detector(vacuum)
-        if solidsToIgnore is None: # by default ignore nothing
+        if solidsToIgnore is None:  # by default ignore nothing
             self.solidsToIgnore = lambda _: False
         else:
-            self.solidsToIgnore=solidsToIgnore
+            self.solidsToIgnore = solidsToIgnore
 
         if noUnion is None:
             self.noUnionClassifier = lambda _: False
@@ -225,8 +216,8 @@ class GDMLLoader:
             v, pos, rot, parent_material_ref = q.pop()
             logger.debug(f"Generating volume {v.name}\tsolid ref: {v.solid_ref}")
             for child, c_pos, c_rot in zip(v.children, v.child_pos, v.child_rot):
-                c_pos = self.get_vals(c_pos) if c_pos is not None else np.zeros(3)
-                c_rot = self.get_vals(c_rot) if c_rot is not None else np.identity(3)
+                c_pos = helper.get_vals(c_pos) if c_pos is not None else np.zeros(3)
+                c_rot = helper.get_vals(c_rot) if c_rot is not None else np.identity(3)
                 c_pos = (rot @ c_pos) + pos
                 x_rot = make_rotation_matrix(c_rot[0], [1, 0, 0])
                 y_rot = make_rotation_matrix(c_rot[1], [0, 1, 0])
@@ -256,12 +247,12 @@ class GDMLLoader:
             if mesh is None:
                 continue
             if classification == 'pmt':
-                channel_type = kwargs.pop('channel_type',None)
+                channel_type = kwargs.pop('channel_type', None)
                 solid = Solid(mesh, **kwargs)
-                detector.add_pmt(solid, displacement=pos, rotation=rot, channel_type=channel_type)   
+                detector.add_pmt(solid, displacement=pos, rotation=rot, channel_type=channel_type)
             elif classification == 'solid':
                 solid = Solid(mesh, **kwargs)
-                detector.add_solid(solid, displacement=pos, rotation=rot)   
+                detector.add_solid(solid, displacement=pos, rotation=rot)
             else:
-                raise Exception('Unknown volume classification: '+classification)
+                raise Exception('Unknown volume classification: ' + classification)
         return detector
